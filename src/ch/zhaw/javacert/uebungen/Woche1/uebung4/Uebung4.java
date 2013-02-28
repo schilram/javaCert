95e18952b80ffaae4f48f8a915f46340de6ffa05
Ramon <schilram@students.zhaw.ch>
2013-02-28 21:13:51 +0100

    Uebung 4 Woche 1

---
+++ src/ch/zhaw/javacert/uebungen/Woche1/uebung4/Uebung4.java
@@ -0,0 +1,55 @@
+package ch.zhaw.javacert.uebungen.Woche1.uebung4;
+/**
+ Schreiben Sie ein kleines Programm bestehend aus einer Klasse, definieren Sie in der Klasse ein Datenfeld vom Typ StringBuilder
+ und ein Datenfeld vom Typ String.
+ Überprüfen Sie in Ihrer Anwendung die Funktionsweise der Methoden der Klassen StringBuilder und String.
+ Führen Sie in einem Loop Konkatenierungs-Operationen aus für StringBuilder und String und vergleichen Sie die Performance der beiden Klassen.
+ **/
+
+/**
+ * @author rschilling
+ */
+public class Uebung4 {
+
+    final static int LOOPS = 100;
+
+    public static void main(String [] args) {
+
+        Long start;
+        Long stop;
+
+        StringBuilder sb = new StringBuilder("Hello");
+        String s = new String("Hello");
+
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < LOOPS; i ++) {
+            sb.append(sb);
+        }
+        stop = System.currentTimeMillis();
+        System.out.println("sb.append(sb): " + diff(start, stop));
+
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < LOOPS; i ++) {
+            s = s + s;
+        }
+        stop = System.currentTimeMillis();
+        System.out.println("s + s: " + diff(start, stop));
+
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < LOOPS; i ++) {
+            s = s.concat(s);
+        }
+        stop = System.currentTimeMillis();
+        System.out.println("s.concat(s): " + diff(start, stop));
+
+    }
+
+    private static long diff(Long start, Long end) {
+        return (end - start)/100;
+    }
+
+
+}
